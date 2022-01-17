e353b81d65e0ae934055a4115dececc3b40c1197
NuminousBlue <jdiehl2236@gmail.com>
2022-01-16 22:43:09 -0600

    fetches & localstorage & html complete and fully functional

--- native.js
+++ native.js
@@ -1,11 +1,42 @@
 // DOM selector variables
-const navClose = $(".closebtn");
 const sidebar = $('.sidebar');
 const hamburger = $('#hamburger');
 const searchFormEl = $('#searchform');
 const searchQueryEl = $('#search-query');
-const cardBodyEl = $('#weather-wrapper');
+const searchInput = $('#search-input')
 const historyEl = $('#history');
+const bigBox = $('#weather-main');
+const bigList = $('#big-list');
+const cityHeader = $('#city-header');
+const dataCol = $('#right-column');
+const searchBtn = $('#searchbtn')
+
+let lastSearched;
+
+
+class Location {
+  constructor(city, date, temp, feel, humidity, wind, uv, url) {
+    this.city = city;
+    this.date = date;
+    this.temp = temp;
+    this.feel = feel;
+    this.humidity = humidity;
+    this.windSpeed = wind;
+    this.uvIndex = uv;
+    this.url = url
+  }
+};
+
+// Handles local storage refresh on window load
+function storageInit() {
+  displayHistory();
+}
+// Initialize page with search history from local storage
+window.onload = () => {
+  event.stopPropagation();
+  storageInit();
+  populateMain();
+}
 
 
 // Sidebar collapse&expand control
@@ -28,22 +59,15 @@ const closeNav = function () {
 $(".closebtn").click(closeNav());
 $(".openbtn").click(openNav());
 
-// Initialize page with search history from local storage
-window.onload = function() {
-  storageInit()
-}
+// window.addEventListener("click", () => {
 
+// })
 
-function storageInit() {
-  displayHistory();
-}
-
-// Handle search submit
+// Handle submission of search and fetch to mapbox
 function getCoords(event) {
   event.preventDefault();
   let searchValue = $('#search-input').val();
   let searchFormatted = searchValue.split(', ');
-  console.log(searchFormatted.length);
   if (searchFormatted.length < 2) {
     alert('Please enter a valid city and state, separated by a comma and a space.  Example: Austin, Texas');
     return;
@@ -54,18 +78,15 @@ function getCoords(event) {
     city = city.replace(/\s+/g, '');
     state = state.replace(/\s+/g, '');
   }
-  console.log(city);
-  console.log(state);
 
+  // Build mapbox fetch url using terms grabbed from search form
   let searchTerms = city + '%20' + state;
   let geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + searchTerms +
     '.json?access_token=pk.eyJ1IjoibnVtaW5vdXNibHVlIiwiYSI6ImNrdWFocWk0NTBoMHkydm1uMHRuODY1aXYifQ.lEnI_ZRd1_I8-2IZUSA_MA';
-  console.log(geoUrl);
 
-
+  // Fetch request to mapbox
   fetch(geoUrl)
     .then(response => {
-      console.log(response);
       if (!response.ok) {
         throw response.json();
       } else {
@@ -76,25 +97,24 @@ function getCoords(event) {
       let lat = location.features[0].geometry.coordinates[1].toString().substring(0, 5);
       let long = location.features[0].geometry.coordinates[0].toString().substring(0, 5);
 
-      console.log(lat);
-      console.log(long);
       searchTerm = location.query.join(', ').toUpperCase();
       searchQueryEl.text(searchTerm);
-
-      buildWeatherUrl(lat, long);
+      
       getWeatherObj(buildWeatherUrl(lat, long));
-      displayHistory();
+
     });
 }
 
+// Build data gathered from mapbox fetch into weathermap url for new fetch
 function buildWeatherUrl(coordsX, coordsY) {
   const APIkey = '3cb8573c763144f45675130c586b0e88';
   let weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + coordsX + '&lon=' + coordsY + '&units=imperial&appid=' + APIkey;
   return weatherUrl;
 }
 
-function getWeatherObj(coordsUrl) {
-  fetch(coordsUrl)
+// Retrieve and format data received from open weather api
+async function getWeatherObj(coordsUrl) {
+  await fetch(coordsUrl)
     .then(response => {
       if (!response.ok) {
         throw response.json();
@@ -105,35 +125,26 @@ function getWeatherObj(coordsUrl) {
     .then(data => {
       let location = searchTerm;
       let objIndex = data.current;
-      console.log(location);
       let day = moment().format('MMM Do YY');
-      console.log(day)
       let temperature = objIndex.temp.toString().substring(0, 4) + '\xB0 F';
-      console.log(temperature)
       let realFeel = objIndex.feels_like.toString().substring(0, 4) + '\xB0 F';
-      console.log(realFeel);
       let humid = objIndex.humidity.toString() + ' %';
-      console.log(humid);
       let windSpeed = objIndex.wind_speed.toString() + ' mph';
-      console.log(windSpeed);
       let uvIndex = objIndex.uvi.toString();
-      console.log(uvIndex);
-
-      const newItem = new Template(location, day, temperature, realFeel, humid, windSpeed, uvIndex);
+      // Send to local storage using search terms as property key and template of data as values
+      const newItem = new Location(location, day, temperature, realFeel, humid, windSpeed, uvIndex, coordsUrl);
+      return newItem;
+    })
+    .then(newItem => {
       localStorage.setItem(searchTerm, JSON.stringify(newItem));
+      lastSearched = searchTerm;
+    })
+    .then(() => {
+      cityHeader.text(' ');
+      dataCol.text(' ');
+      populateMain();
     })
 }
-
-// Object constructor determining format of storage items
-function Template(city, date, temp, feel, humidity, wind, uv) {
-  this.city = city;
-  this.date = date;
-  this.temp = temp;
-  this.feel = feel;
-  this.humidity = humidity;
-  this.windSpeed = wind;
-  this.uvIndex = uv;
-};
 
 
 // Uses local storage to populate search history with previous cities viewed
@@ -144,6 +155,7 @@ function displayHistory() {
       item = JSON.parse(localStorage.getItem(localStorage.key([i])));
       anchor = document.createElement("a")
       node = document.createTextNode(item.city)
+      anchor.setAttribute("href", item.url)
       $('#mySidebar').append(anchor);
       anchor.append(node);
       i++;
@@ -154,4 +166,51 @@ function displayHistory() {
   }
 };
 
+// $("anchor").click((event) => {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   let url = event.target.href;
+//   console.log(event.target)
+//   getWeatherObj(url)
+//     .then(() => {
+//       localStorage.getItem(event.target.innerHTML)
+//     })
+// })
+
+// Handle transfer of data from response/local storage to weather cards
+function populateMain() {
+  // Define condition and function for comparing last search to displayed card
+  
+
+  if (localStorage.length > 0) {
+    let mainKey = localStorage.length - 1;
+    let keyName = localStorage.key(mainKey);
+    let data = JSON.parse(localStorage.getItem(keyName))
+    let values = Object.values(data)
+    values.pop();
+    let cityName = values.shift();
+    cityHeader.text(cityName);
+    let list = document.createElement('ul');
+    dataCol.append(list)
+    values.forEach((value) => {
+      row = document.createElement('li');
+      dataCol.append(row);
+      row.textContent = value;
+    })
+  }
+  return
+};
+
+
+function populateMinor() {
+  if (localStorage.length > 1) {
+
+
+
+
+
+  }
+}
+
 searchFormEl.submit(getCoords);
+
